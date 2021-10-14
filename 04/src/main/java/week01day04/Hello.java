60a01899efa3d34b980d99a6db765069b4aba990
ht0m <hartht@gmail.com>
2021-10-14 20:18:02 +0200

    Added week01day04 package and Hello class

---
+++ 04/src/main/java/week01day04/Hello.java
@@ -0,0 +1,7 @@
+package week01day04;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("1st week, 4th day, 58th 'Hello World!' and counting... :)");
+    }
+}
