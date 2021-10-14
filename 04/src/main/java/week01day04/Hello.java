ded8743217bae53ebc8719e88317a579add009da
ht0m <89078910+ht0m@users.noreply.github.com>
2021-10-14 20:21:39 +0200

    Delete 04/src/main/java/week01day04 directory
    Incorrect dir name (04 vs day04)

--- 04/src/main/java/week01day04/Hello.java
+++
@@ -1,7 +0,0 @@
-package week01day04;
-
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("1st week, 4th day, 58th 'Hello World!' and counting... :)");
-    }
-}
